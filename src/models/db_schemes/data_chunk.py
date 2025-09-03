9b63e8c303f8111343ddf4729ca9fca50cc56000
zizokosaa <zeyadsalih333@gmail.com>
2025-09-03 07:33:55 +0300

    set indecies for projects + chunks

--- src/models/db_schemes/data_chunk.py
+++ src/models/db_schemes/data_chunk.py
@@ -12,3 +12,17 @@ class DataChunk(BaseModel):
 
     class Config:
         arbitrary_types_allowed = True
+
+    @classmethod  # classmethod ==> staticmethod which doesn't need self 
+    def get_indexes(cls):
+        return [
+            {
+                "key": [
+                    ("chunk_project_id", 1)
+                ],
+                "name": "chunk_project_id_index_1",
+                "unique": False  # Ensures that each project_id is unique in the collection
+            }
+        ]
+
+
